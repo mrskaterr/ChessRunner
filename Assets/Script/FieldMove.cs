3158359e7454ba977d5f996f6223846af7174e61
Pawel <mrskatervv@gmail.com>
2021-11-11 00:32:29 +0100

    optimal fiiedmove

--- Assets/Script/FieldMove.cs
+++ Assets/Script/FieldMove.cs
@@ -7,18 +7,22 @@ public class FieldMove : MonoBehaviour
 {
     
     Transform Fields;
+    private const float StartSpeed=0.001f;
+    private int StartHowMany;
     public GameObject Abcdefgh;
     public GameObject ScoreTxt;
     int HowMany;
-    public int Speed;
+    public float Speed;
+
+    
     void Start()
     {
-        
-        HowMany=transform.childCount;
+        StartHowMany=transform.childCount;
+        HowMany=StartHowMany;
+        ScoreTxt.GetComponent<Text>().text=(HowMany-StartHowMany).ToString();
     }
     void Update()
     {
-        ScoreTxt.GetComponent<Text>().text=HowMany.ToString();
         if(Abcdefgh){
             if(Abcdefgh.transform.position.y<=-2){
                 Abcdefgh.gameObject.SetActive(false);
@@ -26,25 +30,29 @@ public class FieldMove : MonoBehaviour
             else
             {
                 Abcdefgh.transform.position=new Vector3(Abcdefgh.transform.position.x,
-                                                        Abcdefgh.transform.position.y-0.001f*Speed,
+                                                        Abcdefgh.transform.position.y-StartSpeed*Speed,
                                                         Abcdefgh.transform.position.z);
             }
         }
         for(int i=0;i<transform.childCount;i++){
             Fields=gameObject.transform.GetChild(i);
-            Fields.position=new Vector3(Fields.position.x,Fields.position.y-0.001f*Speed,Fields.position.z);
+            Fields.position=new Vector3(Fields.position.x,Fields.position.y-StartSpeed*Speed,Fields.position.z);
             if(Fields.position.y<=-1){
                 GetComponent<ChessBoardArray>().UpdateArray();
                 for(int j=0;j<Fields.childCount;j++){
-                    if(Fields.GetChild(j).childCount==1){
+                    if(Fields.GetChild(j).childCount==1)//if chessman on field 
+                    {
                         Destroy(Fields.GetChild(j).GetChild(0).gameObject);
                     }
                 }
                 Fields.position=new Vector3(Fields.position.x,transform.childCount-1,Fields.position.z);
                 GetComponent<SpawnEnemy>().Spawn(Fields);
                 ++HowMany;
+                if(HowMany%10==0){
+                    ++Speed;
+                }
                 Fields.name=HowMany.ToString();
-                ScoreTxt.GetComponent<Text>().text=HowMany.ToString();
+                ScoreTxt.GetComponent<Text>().text=(HowMany-StartHowMany).ToString();
             }
 
         }
